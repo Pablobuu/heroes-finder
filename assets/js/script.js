6f51731773255fa5f4f2fd4dce15693c08ff00ff
Pablo Burgos <pabloburgosc@gmail.com>
2024-03-19 22:18:04 -0300

    estructura lógica inicial,corrección de estilos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,91 @@
 //Validación de formulario
-
-let buscar = document.getElementById("form-busqueda");
-const validar = () => {
-  let patronNum = /^[0-9]{0,3}$/gim;
-  let numIngresado = document.querySelector(".input-busqueda").value;
-  if (!numIngresado.match(patronNum)) {
-    alert("Ingresa sólo números para tu búsqueda");
-    document.querySelector(".input-busqueda").value = "";
-  } else {
-    buscaHeroe(numIngresado);
-  }
-};
-buscar.addEventListener("click", validar);
-
-//AJAX
-
-function buscaHeroe(id) {
-  $.ajax({
-    type: "GET",
-    url: "https://www.superheroapi.com/api.php/4905856019427443/${id}",
-    dataType: "json",
-    success: function (datos) {
-      console.log(datos);
-      muestraInfo;
-      muestraGrafico;
-    },
-    error: function (error) {
-      alert("Error al buscar los datos");
-    },
+$(document).ready(function () {
+  let buscar = document.getElementById("form-busqueda");
+  const validar = () => {
+    let patronNum = /^[0-9]{0,3}$/gim;
+    let numIngresado = document.querySelector(".input-busqueda").value;
+    if (!numIngresado.match(patronNum)) {
+      alert("Ingresa sólo números para tu búsqueda");
+      document.querySelector(".input-busqueda").value = "";
+    } else {
+      buscaHeroe(numIngresado);
+    }
+  };
+  buscar.addEventListener("click", function (event) {
+    event.preventDefault();
+    validar();
   });
-}
+  buscar.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      validar();
+    }
+  });
 
-function muestraInfo(datos) {}
+  //AJAX
 
-function muestraGrafico(datos) {}
+  function muestraInfo(datosApi) {
+    const ul = $("#info-list");
+    $("#nombre").html(datosApi.name);
+    $("#heroes-img").attr("src", datosApi.image.url);
+    $("#aliases").html(datosApi.biography.aliases.join(" | "));
+    ul.empty();
+    ul.append(`<li>Publicado por: ${datosApi.biography.publisher}</li>`);
+    ul.append(
+      `<li>Primera aparición: ${datosApi.biography["first-appearance"]}</li>`
+    );
+    ul.append(`<li>Altura: ${datosApi.appearance.height}</li>`);
+    ul.append(`<li>Peso: ${datosApi.appearance.weight}</li>`);
+    ul.append(`<li>Familia: ${datosApi.connections.relatives}</li>`);
+    ul.append(
+      `<li>Conexiones: ${datosApi.connections["group-affiliation"]}</li>`
+    );
+    ul.append(`<li>Alineamiento: ${datosApi.biography.alignment}</li>`);
+    $("#stats").html("Estadísticas de poder");
+  }
+
+  let dataPoints = [];
+  let options = {
+    title: {
+      text: "",
+    },
+    legend: {},
+    data: [
+      {
+        type: "pie",
+        showInLegend: true,
+        legendText: "{indexLabel}",
+        dataPoints: [""],
+      },
+    ],
+  };
+
+  let chart = new CanvasJS.Chart("chartContainer", options);
+
+  function buscaHeroe(id) {
+    dataPoints = [];
+    options.data[0].dataPoints = dataPoints;
+    chart.render();
+    $.ajax({
+      type: "GET",
+      url: `https://www.superheroapi.com/api.php/4905856019427443/${id}`,
+      dataType: "json",
+      success: function (datosApi) {
+        muestraInfo(datosApi);
+        dataPoints.push(
+          { y: datosApi.powerstats.intelligence, indexLabel: "Intelligence" },
+          { y: datosApi.powerstats.strength, indexLabel: "Strength" },
+          { y: datosApi.powerstats.speed, indexLabel: "Speed" },
+          { y: datosApi.powerstats.durability, indexLabel: "Durability" },
+          { y: datosApi.powerstats.power, indexLabel: "Power" },
+          { y: datosApi.powerstats.combat, indexLabel: "Combat" }
+        );
+        options.data[0].dataPoints = dataPoints;
+        chart.render();
+      },
+      error: function (error) {
+        alert("Error al buscar los datos");
+      },
+    });
+  }
+});
